6597db538353979e233cf1037c6adfd02b67799a
philipotieno <tmitchellb007@gmail.com>
2020-03-13 13:18:12 +0300

    adds location and theme apps

--- api/urls.py
+++ api/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
 ]
